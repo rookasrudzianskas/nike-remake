faf545d811153e703eddf3a694e0273ca02932a6
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 08:57:35 +0200

    Add Nike image
    This commit adds a Nike image to the app.

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import React from "react";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1">
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png",
+        }}
+        style={{width: "100%", aspectRatio: 1}}
+      />
       <StatusBar style="auto" />
     </View>
   );
