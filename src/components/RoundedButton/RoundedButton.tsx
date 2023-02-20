ddcf73a84a43f64e070f4b6ce60faabed260cab4
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 09:45:58 +0200

    feat: add RoundedButton component
    - Add RoundedButton component
    - Add RoundedButton to ProductDetailsScreen

---
+++ src/components/RoundedButton/RoundedButton.tsx
@@ -0,0 +1,24 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+
+
+type Props = {
+  icon: string;
+  action: () => void;
+}
+
+const RoundedButton = ({icon, action}: Props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.7} className="w-9 h-9 flex items-center justify-center bg-gray-900/90 mx-1 rounded-full">
+      {icon === 'share' ? (
+        <Ionicons name="share-outline" size={21} color="white" />
+      ) : (
+        <Ionicons name="ios-close-outline" size={23} color="white" />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default RoundedButton;
--- src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
+++ src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
@@ -1,16 +1,33 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image, FlatList, useWindowDimensions} from 'react-native';
 import products from "../../data/products";
+import RoundedButton from "../../components/RoundedButton/RoundedButton";
 
 const ProductDetailsScreen = () => {
   const product = products[0];
+  const {width} = useWindowDimensions(width);
 
   return (
-    <View>
-      <Text>
-        byrookas 🚀
-      </Text>
+    <View className="pt-24 relative">
+      <FlatList
+        data={product.images}
+        renderItem={({item}) => (
+          <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
+        )}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled={true}
+      />
+      <View className="absolute top-[55px] right-7 flex flex-row items-center">
+        <RoundedButton icon={'share'} />
+        <RoundedButton icon={'somethingElse'} />
+      </View>
+      <View className="px-5">
+        <Text className="text-gray-900 font-[600] text-3xl my-2">{product.name}</Text>
+        <Text className="text-gray-900 font-[600] text-lg">${product.price}</Text>
+        <Text className="text-gray-800 font-[400] mt-3 text-[15px]">{product.description}</Text>
+      </View>
     </View>
   );
 };
