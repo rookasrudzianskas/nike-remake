34ae16519a80fdc17020ed540e2cb97dedbca165
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 09:58:50 +0200

    Add addToCart function to ProductDetailsScreen
    This function will be used to add the product to the cart.

--- src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
+++ src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
@@ -7,10 +7,13 @@ import RoundedButton from "../../components/RoundedButton/RoundedButton";
 const ProductDetailsScreen = () => {
   const product = products[0];
   const {width} = useWindowDimensions(width);
+  const addToCart = () => {
+    console.warn('Added to cart');
+  }
 
   return (
     <View>
-      <ScrollView className="relative" contentContainerStyle={{paddingBottom: 50}}>
+      <ScrollView className="relative pb-20" contentContainerStyle={{paddingBottom: 200}} showsVerticalScrollIndicator={false}>
         <FlatList
           data={product.images}
           renderItem={({item}) => (
@@ -30,7 +33,7 @@ const ProductDetailsScreen = () => {
           <Text className="text-gray-800 font-[400] mt-3 text-[15px]">{product.description}</Text>
         </View>
       </ScrollView>
-      <TouchableOpacity activeOpacity={0.8} className="absolute bg-black bottom-10 py-4 rounded-2xl w-[90%]" style={{alignSelf: 'center', alignItems: 'center'}}>
+      <TouchableOpacity onPress={addToCart} activeOpacity={0.8} className="absolute bg-black bottom-10 py-4 rounded-2xl w-[90%]" style={{alignSelf: 'center', alignItems: 'center'}}>
         <Text className="text-white font-[600]">Add to cart</Text>
       </TouchableOpacity>
     </View>
