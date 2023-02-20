352a9e804217e55cd1ebe0dc069c871a7cf4f319
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 09:19:18 +0200

    Add product screen
    This commit adds a new screen for displaying products.
    
    It also adds a filter button to the top of the screen.

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import {Text, View, Image, FlatList} from 'react-native';
+import { View } from 'react-native';
 import React from "react";
-import products from "./src/data/products";
+import ProductScreen from "./src/screens/ProductScreen";
 
 export default function App() {
   return (
     <View className="flex-1">
-      <FlatList
-        data={products}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item}) => (
-          <View style={{ width: "50%", padding: 1 }}>
-            <Image source={{ uri: item.image }} style={{width: "100%", aspectRatio: 1}} />
-          </View>
-        )}
-      />
-
+      <ProductScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/ProductScreen/ProductScreen.tsx
@@ -0,0 +1,31 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet, Image, FlatList, TouchableOpacity} from 'react-native';
+import products from "../../data/products";
+import {Ionicons} from "@expo/vector-icons";
+// import products from "../../data/products";
+
+const ProductScreen = () => {
+  return (
+    <View className="flex flex-col">
+      <View className="flex flex-row pt-16 items-center px-3 space-x-2 mb-2">
+        <TouchableOpacity activeOpacity={0.7}>
+          <Ionicons name="ios-filter-outline" size={24} color="black" />
+        </TouchableOpacity>
+        <Text className="text-gray-500 font-[500]">Filter by size</Text>
+      </View>
+      <FlatList
+        data={products}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        renderItem={({item}) => (
+          <TouchableOpacity activeOpacity={0.7} style={{ width: "50%", padding: 1 }}>
+            <Image source={{ uri: item.image }} style={{width: "100%", aspectRatio: 1}} />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
+
+export default ProductScreen;
