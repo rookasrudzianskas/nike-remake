c43326636d20aa384958c4685cf52ac1a7c65f23
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 09:05:50 +0200

    fix

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
+import {Text, View, Image, FlatList} from 'react-native';
 import React from "react";
+import products from "./src/data/products";
 
 export default function App() {
   return (
     <View className="flex-1">
-      <Image
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png",
-        }}
-        style={{width: "100%", aspectRatio: 1}}
+      <FlatList
+        data={products}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <View style={{ width: "50%", padding: 1 }}>
+            <Image source={{ uri: item.image }} style={{width: "100%", aspectRatio: 1}} />
+          </View>
+        )}
       />
+
       <StatusBar style="auto" />
     </View>
   );
