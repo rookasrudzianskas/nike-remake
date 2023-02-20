00c6ff14b184b397eb3ba697a04ab77fa8e5eb4a
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 10:00:52 +0200

    Add buttons to product details screen
    The buttons were missing from the product details screen.
    
    This commit adds them.

--- src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
+++ src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
@@ -23,16 +23,16 @@ const ProductDetailsScreen = () => {
           showsHorizontalScrollIndicator={false}
           pagingEnabled={true}
         />
-        <View className="absolute top-[55px] right-7 flex flex-row items-center">
-          <RoundedButton icon={'share'} />
-          <RoundedButton icon={'somethingElse'} />
-        </View>
         <View className="px-5">
           <Text className="text-gray-900 font-[600] text-3xl my-2">{product.name}</Text>
           <Text className="text-gray-900 font-[600] text-lg">${product.price}</Text>
           <Text className="text-gray-800 font-[400] mt-3 text-[15px]">{product.description}</Text>
         </View>
       </ScrollView>
+      <View className="absolute top-[55px] right-7 flex flex-row items-center">
+        <RoundedButton icon={'share'} />
+        <RoundedButton icon={'somethingElse'} />
+      </View>
       <TouchableOpacity onPress={addToCart} activeOpacity={0.8} className="absolute bg-black bottom-10 py-4 rounded-2xl w-[90%]" style={{alignSelf: 'center', alignItems: 'center'}}>
         <Text className="text-white font-[600]">Add to cart</Text>
       </TouchableOpacity>
