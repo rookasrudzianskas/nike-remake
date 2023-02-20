9b7940675f420d8e490e72ff846ae0e68962d5a3
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 11:37:19 +0200

    chore: add redux
    - Add redux and redux-thunk
    - Add react-redux
    - Add @types/react-redux
    - Add @types/hoist-non-react-statics
    - Add @types/use-sync-external-store
    - Add @types/prop-types
    - Add @types/scheduler
    - Add csstype
    - Add immer
    - Add hoist-non-react-statics
    - Add use-sync-external-store

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import React from "react";
-import ProductScreen from "./src/screens/ProductScreen";
-import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
-import ShoppingCart from "./src/screens/ShoppingCart";
 import Navigation from "./src/navigation/navigation";
+import {Provider} from "react-redux";
+import {store} from "./src/store";
 
 export default function App() {
   return (
-    <View className="flex-1">
-      <Navigation />
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View className="flex-1">
+        <Navigation />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
