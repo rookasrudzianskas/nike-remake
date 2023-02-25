f2cc57a42671b802a01d817f5de339ba9e788ff8
Rokas <rookas.rudzenskas@gmail.com>
2023-02-25 12:18:21 +0200

    feat: add order creation
    Add a mutation to create an order.

--- src/store/apiSlice.tsx
+++ src/store/apiSlice.tsx
@@ -13,7 +13,18 @@ export const apiSlice = createApi({
     getProduct: builder.query({
       query: (id) => `products/${id}`,
     }),
+    // Orders
+    createOrder: builder.mutation({
+      query: (newOrder) => ({
+        url: 'orders',
+        method: 'POST',
+        body: newOrder,
+      }),
+    }),
+    getOrder: builder.query({
+      query: (ref) => `orders/${ref}`,
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProductQuery } = apiSlice;
+export const { useGetProductsQuery, useGetProductQuery, useCreateOrderMutation, useGetOrderQuery } = apiSlice;
