2dfaf5b8edef72ea78913edfb44f20a54b1fa34a
Rokas <rookas.rudzenskas@gmail.com>
2023-02-25 12:37:11 +0200

    Add order creation functionality
    - Added order creation functionality
    - Added order reference to the alert
    - Cleared the cart after the order has been created

--- src/screens/ShoppingCart/ShoppingCart.tsx
+++ src/screens/ShoppingCart/ShoppingCart.tsx
@@ -1,19 +1,42 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, FlatList, TouchableOpacity, Alert} from 'react-native';
 import CartListItem from "../../components/CartListItem";
 import {Ionicons} from "@expo/vector-icons";
 import {useNavigation} from "@react-navigation/native";
-import {useSelector} from "react-redux";
-import {selectDeliveryPrice, selectSubtotal, selectTotal} from "../../store/cartSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {cartSlice, selectDeliveryPrice, selectSubtotal, selectTotal} from "../../store/cartSlice";
 import {useCreateOrderMutation} from "../../store/apiSlice";
 
 const ShoppingCart = () => {
   const navigation = useNavigation();
   const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch(cartSlice);
   const [createOrder, {data, error, isLoading}] = useCreateOrderMutation();
-  const addToCart = () => {
+  const subTotal = useSelector(selectSubtotal);
+  const deliveryFee = useSelector(selectDeliveryPrice);
+  const total = useSelector(selectTotal);
+  const onCreateOrder = async () => {
+    const result = await createOrder({
+      items: cartItems,
+      subTotal,
+      deliveryFee,
+      customer: {
+        name: "Rokas",
+        address: 'My Home',
+        phone: '123456789',
+        email: 'myemail'
+      }
+    });
 
+    if(result.data?.status === 'OK') {
+      console.log(result.data);
+      Alert.alert(
+        'Order has been submitted',
+        `Your order reference is: ${result.data.data.ref}`
+      );
+      dispatch(cartSlice.actions.clear());
+    }
   }
 
   return (
@@ -54,7 +77,7 @@ const ShoppingCart = () => {
         </View>
       )}
 
-      <TouchableOpacity onPress={addToCart} activeOpacity={0.8} className="absolute bg-black bottom-10 py-4 rounded-2xl w-[90%]" style={{alignSelf: 'center', alignItems: 'center'}}>
+      <TouchableOpacity onPress={onCreateOrder} activeOpacity={0.8} className="absolute bg-black bottom-10 py-4 rounded-2xl w-[90%]" style={{alignSelf: 'center', alignItems: 'center'}}>
         <Text className="text-white font-[600]">Add to cart</Text>
       </TouchableOpacity>
     </View>
