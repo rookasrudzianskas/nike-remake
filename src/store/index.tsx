5be01cc4f9982e99227b5174bc2b90ace2510648
Rokas <rookas.rudzenskas@gmail.com>
2023-02-25 11:22:47 +0200

    feat: add apiSlice
    - add apiSlice
    - add middleware

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {productsSlice} from "./productsSlide";
 import {cartSlice} from "./cartSlice";
+import { apiSlice } from './apiSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsSlice.reducer,
     cart: cartSlice.reducer,
+    api: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
